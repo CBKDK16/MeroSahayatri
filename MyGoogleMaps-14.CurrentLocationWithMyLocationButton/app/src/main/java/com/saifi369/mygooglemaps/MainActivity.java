bcda61935840cacf23de1450890e965735d56f95
manish <githubmanish16@gmail.com>
2022-05-21 10:33:34 +0545

    our location

---
+++ MyGoogleMaps-14.CurrentLocationWithMyLocationButton/app/src/main/java/com/saifi369/mygooglemaps/MainActivity.java
@@ -0,0 +1,298 @@
+package com.saifi369.mygooglemaps;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    public static final int DEFAULT_ZOOM = 15;
+    private final double ISLAMABAD_LAT = 27.663597485639542;
+    private final double ISLAMABAD_LNG = 85.31926805374955;
+
+    public static final int PERMISSION_REQUEST_CODE = 9001;
+    private static final int PLAY_SERVICES_ERROR_CODE = 9002;
+    public static final int GPS_REQUEST_CODE = 9003;
+    public static final String TAG = "MapDebug";
+    private boolean mLocationPermissionGranted;
+
+    private ImageButton mBtnLocate;
+    private GoogleMap mGoogleMap;
+    private EditText mSearchAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mSearchAddress = findViewById(R.id.et_address);
+        mBtnLocate = findViewById(R.id.btn_locate);
+        mBtnLocate.setOnClickListener(this::geoLocate);
+
+        initGoogleMap();
+
+    }
+
+    private void geoLocate(View view) {
+        hideSoftKeyboard(view);
+
+        String locationName = mSearchAddress.getText().toString();
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            List<Address> addressList = geocoder.getFromLocationName(locationName,1);
+
+            if (addressList.size() > 0) {
+                Address address = addressList.get(0);
+
+                gotoLocation(address.getLatitude(), address.getLongitude());
+
+                showMarker(address.getLatitude(), address.getLongitude());
+
+                Toast.makeText(this, address.getLocality(), Toast.LENGTH_SHORT).show();
+
+                Log.d(TAG, "geoLocate: Locality: " + address.getLocality());
+            }
+
+        } catch (IOException e) {
+
+
+        }
+
+
+    }
+
+    private void showMarker(double lat, double lng) {
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(new LatLng(lat, lng));
+        mGoogleMap.addMarker(markerOptions);
+    }
+
+    private void hideSoftKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    private void initGoogleMap() {
+
+        if (isServicesOk()) {
+            if (isGPSEnabled()) {
+                if (checkLocationPermission()) {
+                    Toast.makeText(this, "Ready to Map", Toast.LENGTH_SHORT).show();
+
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.map_fragment_container);
+
+                    supportMapFragment.getMapAsync(this);
+                } else {
+                    requestLocationPermission();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: map is showing on the screen");
+
+        mGoogleMap = googleMap;
+        gotoLocation(ISLAMABAD_LAT, ISLAMABAD_LNG);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mGoogleMap.setMyLocationEnabled(true);
+
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        mGoogleMap.getUiSettings().setMapToolbarEnabled(true);
+
+
+    }
+
+    private void gotoLocation(double lat,double lng){
+
+        LatLng latLng=new LatLng(lat,lng);
+
+        CameraUpdate cameraUpdate= CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM);
+
+        mGoogleMap.moveCamera(cameraUpdate);
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+    }
+
+    private boolean isGPSEnabled() {
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        boolean providerEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (providerEnabled) {
+            return true;
+        } else {
+
+            AlertDialog alertDialog = new AlertDialog.Builder(this)
+                    .setTitle("GPS Permissions")
+                    .setMessage("GPS is required for this app to work. Please enable GPS.")
+                    .setPositiveButton("Yes", ((dialogInterface, i) -> {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(intent, GPS_REQUEST_CODE);
+                    }))
+                    .setCancelable(false)
+                    .show();
+
+        }
+
+        return false;
+    }
+
+    private boolean checkLocationPermission() {
+
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean isServicesOk() {
+
+        GoogleApiAvailability googleApi = GoogleApiAvailability.getInstance();
+
+        int result = googleApi.isGooglePlayServicesAvailable(this);
+
+        if (result == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (googleApi.isUserResolvableError(result)) {
+            Dialog dialog = googleApi.getErrorDialog(this, result, PLAY_SERVICES_ERROR_CODE, task ->
+                    Toast.makeText(this, "Dialog is cancelled by User", Toast.LENGTH_SHORT).show());
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Play services are required by this application", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id){
+
+            case R.id.maptype_none:{
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+            }
+            case R.id.maptype_normal:{
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            }
+            case R.id.maptype_satellite:{
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            }
+            case R.id.maptype_terrain:{
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            }
+            case R.id.maptype_hybrid:{
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GPS_REQUEST_CODE) {
+
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            boolean providerEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            if (providerEnabled) {
+                Toast.makeText(this, "GPS is enabled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "GPS not enabled. Unable to show user location", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
