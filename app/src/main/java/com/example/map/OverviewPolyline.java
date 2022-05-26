f8b8bbea9485ce35d6ccc56727bafcab816b0274
manish <githubmanish16@gmail.com>
2022-05-26 10:01:24 +0545

    second

---
+++ app/src/main/java/com/example/map/OverviewPolyline.java
@@ -0,0 +1,16 @@
+package com.example.map;
+
+public class OverviewPolyline {
+
+    private String points;
+
+    public String getPoints()
+    {
+        return points;
+    }
+
+    public void setPoints(String points)
+    {
+        this.points = points;
+    }
+}
